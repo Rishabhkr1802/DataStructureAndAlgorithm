f9ba50668414477f8ab0467d43033d7a5faa267a
Rishabhkr1802 <krrishabh1802@gmail.com>
2025-11-06 20:41:58 +0530

    This is test commit

---
+++ C++/Sum copy.cpp
@@ -0,0 +1,11 @@
+// program for sum of two number
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1, b = 3;
+    int sum = a + b;
+    cout << "Sum of two number's is : " << sum;
+    return 0;
+}
